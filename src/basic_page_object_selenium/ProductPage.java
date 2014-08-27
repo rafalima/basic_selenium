c9cfff4080d64533c4d3a8d271376804285eb01d
Rafael <rafalima07@gmail.com>
2014-08-27 13:24:43 -0300

    adicionando testes com Page Object

---
+++ src/basic_page_object_selenium/ProductPage.java
@@ -0,0 +1,49 @@
+package basic_page_object_selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPage {
+	
+	private WebDriver driver;
+	
+	private static final By productsMenuLink = By.linkText("Products");
+	private static final By newProductBtn = By.linkText("New Product");
+	private static final By productTitle = By.id("product_title");
+	private static final By productPrice = By.id("product_price");
+	private static final By productImage = By.id("product_image_file_name");
+	private static final By productSubmit = By.id("product_submit");
+	private static final By mensagemUsuario = By.className("flash_notice");
+	private static final By camposObrigatorios = By.className("inline-errors"); 
+	
+	public ProductPage(WebDriver webDriver) {
+		driver = webDriver;
+	}
+	
+	public void acessaPaginaNovoProduto() {
+		driver.findElement(productsMenuLink).click();
+		driver.findElement(newProductBtn).click();
+	}
+	
+	public void novoProduto(String titulo, String preco, String imagem) {
+		driver.findElement(productTitle).sendKeys(titulo);
+		driver.findElement(productPrice).sendKeys(preco);
+		driver.findElement(productImage).sendKeys(imagem);
+		driver.findElement(productSubmit).click();		
+	}
+	
+	public boolean mensagemFoiMostrada(String mensagemEsperada) {
+		String mensagemAchada = driver.findElement(mensagemUsuario).getText();
+		return mensagemAchada.equals(mensagemEsperada);
+	}
+	
+	public boolean mensagemCampoObrigatorioMostrado(String mensagemEsperada) {
+		String mensagemAchada = driver.findElement(camposObrigatorios).getText();
+		return mensagemAchada.equals(mensagemEsperada);
+	}
+	
+	
+		
+	
+
+}
---
+++ src/basic_page_object_selenium/TestComPageObject.java
@@ -0,0 +1,36 @@
+package basic_page_object_selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestComPageObject extends Driver {
+	
+	
+	@Before
+	public void acessaPaginaAdmin() {
+		driver.get(adminUrl);
+	}
+	
+	@Test
+	public void criarNovoProduto() {
+		String mensagemEsperada = "Product was successfully created.";
+		
+		ProductPage productPage = new ProductPage(driver);
+		productPage.acessaPaginaNovoProduto();
+		productPage.novoProduto("producto PO", "10", "darkside.png");
+		assertTrue(productPage.mensagemFoiMostrada(mensagemEsperada));	
+	}
+	
+	@Test
+	public void criarNovoProdutoSemCamposObrigatorios() {
+		String mensagemEsperada = "Can't Be Blank";
+		
+		ProductPage productPage = new ProductPage(driver);
+		productPage.acessaPaginaNovoProduto();
+		productPage.novoProduto("novo Produto", "10", "");
+		assertTrue(productPage.mensagemCampoObrigatorioMostrado(mensagemEsperada));
+	}
+
+}
