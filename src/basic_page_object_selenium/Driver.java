32e51c239ae13b14c3423700ca0aa7bc491cf17e
Rafael <rafalima07@gmail.com>
2014-08-27 10:49:28 -0300

    Testes sem Page Object

--- src/basic_page_object_selenium/Driver.java
+++ src/basic_page_object_selenium/Driver.java
@@ -1,25 +1,35 @@
 package basic_page_object_selenium;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TestPreparation {
+public class Driver {
 	
 	public WebDriver driver;
+	public static int DEFAULT_WAIT_TIME = 5;
+	public String adminUrl = "http://localhost:3000/admin";
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("driver setup");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		driver.quit();
 	}
+	
+	public WebElement waitForElement(By by) {
+		return (new WebDriverWait(driver,DEFAULT_WAIT_TIME).until(ExpectedConditions.presenceOfElementLocated(by)));
+	}
+	
+	
 	
 }
